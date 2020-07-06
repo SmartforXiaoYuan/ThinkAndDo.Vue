2d7ffb6348bf2107309bb470819dd2a35157e1ee
yoga <799786684@qq.com>
2020-07-06 22:27:44 +0800

    项目起步

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Index from '../views/Home/Index.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes=   [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/', // 首页
+    name: 'Index',
+    component: Index
+    // component: () => import('../views/Home/Index.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/goodsList', // 商品列表
+    name: 'GoodsList',
+    component: () => import('../views/Goods/GoodsList.vue')
+  },
 ]
+
+//   const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: Home
+//   },
+//   {
+//     path: '/about',
+//     name: 'About',
+//     component: () => import('../views/About.vue')
+//   }
+// ]
 
 const router = new VueRouter({
   routes
---
+++ src/views/Home/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="homePage">
+   韩总的电商首页
+  </div>
+</template>
+
+<script>
+ 
+
+export default {
+  name: 'Index'
+}
+</script>
